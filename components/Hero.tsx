93fdd21ba4b44a5ad9170a0fd1f64696ed81dcd2
Goran Kostic <damijan0034@gmail.com>
2024-02-22 17:52:25 +0100

    add noStore

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,16 @@
-import { fetchTrending } from "@actions/movieData"
-import HeroCard from "./HeroCard"
+import { fetchTrending } from '@/actions/movieData'
+import { unstable_noStore as noStore } from 'next/cache'
 
+import HeroCard from './HeroCard'
 
 const Hero =async () => {
+ noStore()
     const trending=await fetchTrending()
+
     const randomNumber=Math.floor(Math.random() * trending.length)
+    console.log(randomNumber)
     const trendingMovie=trending[randomNumber]
+    console.log(trendingMovie)
   return (
     <div>
         <HeroCard trendingMovie={trendingMovie} />
